92299dba534ad8fcecf2b4f9fc09623c36d1e8ad
Dang Mai <contact@dangmai.net>
2018-08-11 17:11:48 -0400

    Init commit

---
+++ packages/apex-ast-serializer/README.md
@@ -0,0 +1,9 @@
+# Apex AST Serializer
+
+This program serializes the Abstract Syntax Tree of an Apex Class/Trigger,
+using [PMD](https://pmd.github.io/) Apex parser.
+The result is printed out to `stdout` as a JSON object.
+
+## License
+
+MIT
---
+++ packages/apex-ast-serializer/src/main/java/net/dangmai/serializer/Apex.java
@@ -0,0 +1,41 @@
+package net.dangmai.serializer;
+
+import com.gilecode.yagson.YaGson;
+import com.gilecode.yagson.YaGsonBuilder;
+import net.sourceforge.pmd.lang.apex.*;
+import net.sourceforge.pmd.lang.ast.Node;
+import org.apache.commons.cli.*;
+
+import java.io.*;
+
+public class Apex {
+    public static void main(String[] args) throws ParseException, FileNotFoundException {
+        Options cliOptions = new Options();
+        cliOptions.addOption("l", "location", true, "Location of Apex class file. If not specified, the Apex content will be read from stdin.");
+        cliOptions.addOption("p", "pretty", false, "Pretty print output JSON.");
+        cliOptions.addOption("h", "help", false, "Print help information.");
+
+        CommandLineParser cliParser = new DefaultParser();
+        CommandLine cmd = cliParser.parse(cliOptions, args);
+        Reader apexReader;
+
+        if (cmd.hasOption("h")) {
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.printHelp("apex-ast-serializer", cliOptions);
+        } else {
+            if (cmd.hasOption("l")) {
+                apexReader = new FileReader(cmd.getOptionValue("l"));
+            } else {
+                apexReader = new BufferedReader(new InputStreamReader(System.in));
+            }
+            ApexParser parser = new ApexParser(new ApexParserOptions());
+            Node topRootNode = parser.parse("", apexReader);
+
+            YaGsonBuilder builder = new YaGsonBuilder();
+            if (cmd.hasOption("p")) {
+                builder.setPrettyPrinting();
+            }
+            System.out.print(builder.create().toJson(topRootNode));
+        }
+    }
+}
