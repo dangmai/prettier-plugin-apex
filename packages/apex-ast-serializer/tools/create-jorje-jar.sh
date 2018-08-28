9f80a6e4ea959ed0d28ddf7c9981b7815b23c78b
Dang Mai <contact@dangmai.net>
2018-08-27 21:50:46 -0400

    Get and use our own jorje jar instead of depending on PMD

---
+++ packages/apex-ast-serializer/tools/create-jorje-jar.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+URL=https://github.com/forcedotcom/salesforcedx-vscode/blob/develop/packages/salesforcedx-vscode-apex/out/apex-jorje-lsp.jar?raw=true
+FILENAME=apex-jorje-lsp-original.jar
+FILENAME_MINIMIZED=apex-jorje-lsp.jar
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+function install() {
+    mv ${FILENAME_MINIMIZED} "${CURRENT_DIR}/../libs/"
+}
+
+function download() {
+    curl -L -o ${FILENAME} ${URL}
+}
+
+
+function minimize() {
+    unzip -d temp ${FILENAME}
+    pushd temp
+    find . -not -path "." \
+        -and -not -path ".." \
+        -and -not -path "./apex*" \
+        -and -not -path "./StandardApex*" \
+        -and -not -path "./messages*" \
+        -and -not -path "./com" \
+        -and -not -path "./com/google" \
+        -and -not -path "./com/google/common*" \
+        -and -not -path "./org" \
+        -and -not -path "./org/antlr" \
+        -and -not -path "./org/antlr/runtime*" \
+        -print0 | xargs -0 rm -rf
+    popd
+    jar -cvf ${FILENAME_MINIMIZED} -C temp/ .
+    rm -rf temp
+}
+
+function cleanup() {
+    rm ${FILENAME}
+}
+
+download
+minimize
+install
+cleanup
