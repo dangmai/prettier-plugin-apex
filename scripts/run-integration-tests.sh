c017b7ebbad8b1704a47f21cb594a462c05e2b2f
Dang Mai <contact@dangmai.net>
2019-06-02 00:15:44 -0400

    WIP - Integration tests with popular Apex packages

---
+++ scripts/run-integration-tests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+mapfile repos <<EOF
+https://github.com/financialforcedev/apex-mdapi.git
+https://github.com/afawcett/declarative-lookup-rollup-summaries.git
+https://github.com/SalesforceFoundation/Cumulus.git
+https://github.com/financialforcedev/fflib-apex-common.git
+https://github.com/kevinohara80/sfdc-trigger-framework.git
+https://github.com/financialforcedev/fflib-apex-mocks.git
+EOF
+
+for repo in ${repos[@]}; do
+  rm -rf repo
+  git clone --depth 1 "$repo" repo
+  npm run debug-check -- "./repo/**/*.{trigger,cls}"
+done
