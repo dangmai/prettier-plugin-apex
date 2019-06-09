86fa69a8ccc1e4f3ba045690512efe800bb1d2be
Dang Mai <contact@dangmai.net>
2019-06-09 15:14:45 -0400

    WIP - Explicit directory for classes/triggers

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -13,5 +13,5 @@ EOF
 for repo in ${repos[@]}; do
   rm -rf repo
   git clone --depth 1 "$repo" repo
-  npm run debug-check -- "./repo/**/*.{trigger,cls}"
+  npm run debug-check -- "./repo/**/classes/*.cls" "./repo/**/triggers/*.trigger"
 done
