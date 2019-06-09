a0e2553af9ce3a29c661f9396c87137ad3df0ef7
Dang Mai <contact@dangmai.net>
2019-06-09 02:04:13 -0400

    Add ability to remotely shut down server

--- packages/apex-ast-serializer/src/main/java/net/dangmai/serializer/server/HttpServer.java
+++ packages/apex-ast-serializer/src/main/java/net/dangmai/serializer/server/HttpServer.java
@@ -6,6 +6,8 @@ import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ShutdownHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.servlet.ServletContainer;
@@ -19,6 +21,8 @@ public class HttpServer {
     public static void main(String[] args) throws Exception {
         Options cliOptions = new Options();
         cliOptions.addOption("p", "port", true, "The port to listen to");
+        cliOptions.addOption("s", "shutdown", false, "Allow the server to be remotely shut down");
+        cliOptions.addOption("a", "password", true, "Password to remotely shut down server");
 
         CommandLineParser cliParser = new DefaultParser();
         CommandLine cmd = cliParser.parse(cliOptions, args);
@@ -27,16 +31,20 @@ public class HttpServer {
         Server server = new Server(port);
 
         ServletContextHandler servletContextHandler = new ServletContextHandler(NO_SESSIONS);
-
-        servletContextHandler.setContextPath("/");
-        server.setHandler(servletContextHandler);
-
+        servletContextHandler.setContextPath("/api");
         ServletHolder servletHolder = servletContextHandler.addServlet(ServletContainer.class, "/ast/*");
         servletHolder.setInitOrder(0);
         servletHolder.setInitParameter(
                 "jersey.config.server.provider.packages",
                 "net.dangmai.serializer.server.resources"
         );
+
+        HandlerList handlerList = new HandlerList();
+        handlerList.addHandler(servletContextHandler);
+        if (cmd.hasOption("s")) {
+            handlerList.addHandler(new ShutdownHandler(cmd.getOptionValue("a")));
+        }
+        server.setHandler(handlerList);
 
         try {
             server.start();
