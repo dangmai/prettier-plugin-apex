d9c5d160a88b4aca9faa33b0102449acc498ce30
Dang Mai <contact@dangmai.net>
2021-05-09 13:03:24 -0400

    Add tests for parser

--- jest.config.standalone.js
+++ jest.config.standalone.js
@@ -5,7 +5,6 @@ module.exports = {
   displayName: "test",
   setupFiles: ["<rootDir>/tests_config/run-spec.js"],
   snapshotSerializers: ["<rootDir>/tests_config/raw-serializer.js"],
-  testRegex: "jsfmt\\.spec\\.js$|__tests__/.*\\.js$",
   testPathIgnorePatterns: [],
   collectCoverage: ENABLE_COVERAGE,
   collectCoverageFrom: [
---
+++ tests/parser/parser.spec.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const path = require("path");
+const prettier = require("prettier");
+
+describe("Parser Tests", () => {
+  it("runs synchronous parser correctly", () => {
+    const fileName = path.join(__dirname, "ValidClass.cls");
+    const source = fs.readFileSync(fileName, "utf8").replace(/\r\n/g, "\n");
+    const output = prettier.format(source, {
+      plugins: ["."],
+      filepath: fileName,
+      parser: "apex",
+    });
+    expect(output).toBeDefined();
+  });
+  it("runs asynchronous parser correctly", () => {
+    const fileName = path.join(__dirname, "ValidClass.cls");
+    const source = fs.readFileSync(fileName, "utf8").replace(/\r\n/g, "\n");
+    const output = prettier.format(source, {
+      plugins: ["."],
+      filepath: fileName,
+      parser: "apex",
+      apexStandaloneParser: "built-in",
+      apexStandalonePort: 2117,
+      apexStandaloneHost: "localhost",
+    });
+    expect(output).toBeDefined();
+  });
+  it("throws error when asynchronous parser server cannot be reached", () => {
+    const fileName = path.join(__dirname, "ValidClass.cls");
+    const source = fs.readFileSync(fileName, "utf8").replace(/\r\n/g, "\n");
+    expect(() =>
+      prettier.format(source, {
+        plugins: ["."],
+        filepath: fileName,
+        parser: "apex",
+        apexStandaloneParser: "built-in",
+        apexStandalonePort: 2118,
+        apexStandaloneHost: "localhost",
+      }),
+    ).toThrow("Failed to connect to Apex parsing server");
+  });
+  it("throws error when input file is invalid", () => {
+    const fileName = path.join(__dirname, "InvalidClass.cls");
+    const source = fs.readFileSync(fileName, "utf8").replace(/\r\n/g, "\n");
+    expect(() =>
+      prettier.format(source, {
+        plugins: ["."],
+        filepath: fileName,
+        parser: "apex",
+        apexStandaloneParser: "built-in",
+        apexStandalonePort: 2117,
+        apexStandaloneHost: "localhost",
+      }),
+    ).toThrow("Unexpected token");
+  });
+});
