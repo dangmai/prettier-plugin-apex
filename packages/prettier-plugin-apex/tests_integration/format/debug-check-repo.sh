718de5e47f330d29819322aeac26c1a32879679a
Dang Mai <contact@dangmai.net>
2024-02-20 19:16:30 -0800

    [debug] try fixing relative path issue

--- packages/prettier-plugin-apex/tests_integration/format/debug-check-repo.sh
+++ packages/prettier-plugin-apex/tests_integration/format/debug-check-repo.sh
@@ -2,6 +2,9 @@
 
 set -euxo pipefail
 
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$CURRENT_DIR/../.."
+
 # This script checkouts a git repository and runs Prettier debug-check on it.
 # It takes 1 argument: the git repository location
 # If environment variable CLONE_DIR is defined, the repositories will be cloned
@@ -13,8 +16,5 @@ rm -rf "$location/$name"
 git clone --depth 1 "$1" "$location/$name"
 # Here we use --no-config so that Prettier does not try to use the test repos
 # Prettier config, as they may interfere with the test runs.
-ls -alh
-ls -alh dist
-ls -alh src
 pnpm --filter=prettier-plugin-apex run debug-check:built-in "$location/$name/**/{classes,triggers}/*.{cls,trigger}" --no-config --ignore-path=./tests_integration/format/ignore-list.txt
 rm -rf "$location/$name"
