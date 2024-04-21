27539bfefd6b5e6dc795f1946cbb381904b95a63
Dang Mai <contact@dangmai.net>
2024-04-21 10:03:03 -0700

    [misc] remove undici type workaround

--- packages/prettier-plugin-apex/typings/global.d.ts
+++ packages/prettier-plugin-apex/typings/global.d.ts
@@ -1,11 +1,4 @@
 import "prettier";
-// eslint-disable-next-line import/no-extraneous-dependencies -- temporary workaround, only needed in dev
-import {
-  type FormData as FormDataType,
-  type Headers as HeadersType,
-  type Request as RequestType,
-  type Response as ResponseType,
-} from "undici";
 
 declare module "prettier" {
   interface RequiredOptions {
@@ -16,30 +9,11 @@ declare module "prettier" {
     apexInsertFinalNewline: boolean;
   }
   namespace __debug {
+    // eslint-disable-next-line import/prefer-default-export
     export function parse(
       originalText: string,
       originalOptions: Partial<RequiredOptions>,
       { massage: boolean },
     ): AST;
   }
-}
-
-// Needed because of this issue: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/60924
-declare global {
-  // Re-export undici fetch function and various classes to global scope.
-  // These are classes and functions expected to be at global scope according to Node.js v18 API
-  // documentation.
-  // See: https://nodejs.org/dist/latest-v18.x/docs/api/globals.html
-  export const {
-    FormData,
-    Headers,
-    Request,
-    Response,
-    fetch,
-  }: typeof import("undici");
-
-  type FormData = FormDataType;
-  type Headers = HeadersType;
-  type Request = RequestType;
-  type Response = ResponseType;
 }
