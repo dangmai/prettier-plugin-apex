85e0f1c79313e65f21c29226d02e0982d156421c
Dang Mai <contact@dangmai.net>
2024-04-21 22:23:53 -0700

    [codespaces] fix JDK location

--- .devcontainer/set-java-home.sh
+++ .devcontainer/set-java-home.sh
@@ -4,7 +4,12 @@ set -euxo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SETTINGS_LOCATION="${SCRIPT_DIR}/../.vscode/settings.json"
+TOOL_VERSIONS_LOCATION="${SCRIPT_DIR}/../.tool-versions"
+JAVA_HOME="~/.asdf/installs/java/$(cat "${TOOL_VERSIONS_LOCATION}" | grep "java" | cut -d' ' -f2)"
 if [ -f "${SETTINGS_LOCATION}" ];
 then
-  jq --arg java_home "$JAVA_HOME" '.salesforcedx-vscode-apex.java.home = $java_home' "${SETTINGS_LOCATION}"
+  jq --arg java_home "$JAVA_HOME" \
+    '."salesforcedx-vscode-apex.java.home" = $java_home | ."java.import.gradle.java.home" = $java_home' \
+    "${SETTINGS_LOCATION}" > settings.tmp.json \
+    && mv settings.tmp.json "${SETTINGS_LOCATION}"
 fi
