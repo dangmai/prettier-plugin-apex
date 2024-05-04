4a33b684178765357bece3fd2907ec7abfa140dd
Dang Mai <contact@dangmai.net>
2024-05-04 09:57:25 -0700

    [nx] cross-platform gradle run

---
+++ packages/apex-ast-serializer/tools/run-gradle.mjs
@@ -0,0 +1,14 @@
+#!/usr/bin/env zx
+import { $, usePowerShell } from "zx";
+
+// This script runs Gradle with the given arguments in a cross-platform way.
+// It's meant to be called from nx run-commands.
+
+let command = "./gradlew";
+
+if (process.platform === "win32") {
+  usePowerShell();
+  command += ".bat";
+}
+const args = process.argv.slice(2);
+await $`${command} ${args}`;
