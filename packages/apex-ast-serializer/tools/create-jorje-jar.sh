b39288c996ff2f6184eb6ed6c03427ea4368b0e7
Dang Mai <contact@dangmai.net>
2024-06-30 14:26:19 -0700

    [jorje] fix indentation

--- packages/apex-ast-serializer/tools/create-jorje-jar.sh
+++ packages/apex-ast-serializer/tools/create-jorje-jar.sh
@@ -11,47 +11,47 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 CURRENT_MD5="$(cat "${CURRENT_DIR}/${CURRENT_MD5_FILENAME}")"
 
 function install() {
-    mv ${FILENAME_MINIMIZED} "${CURRENT_DIR}/../libs/"
+  mv ${FILENAME_MINIMIZED} "${CURRENT_DIR}/../libs/"
 }
 
 function download() {
-    curl -L -o ${FILENAME} ${URL}
+  curl -L -o ${FILENAME} ${URL}
 }
 
 function minimize() {
-    unzip -d temp ${FILENAME}
-    pushd temp
-    find . -not -path "." \
-        -and -not -path ".." \
-        -and -not -path "./apex*" \
-        -and -not -path "./StandardApex*" \
-        -and -not -path "./messages*" \
-        -and -not -path "./com" \
-        -and -not -path "./com/google" \
-        -and -not -path "./com/google/common*" \
-        -and -not -path "./com/google/inject*" \
-        -and -not -path "./javax" \
-        -and -not -path "./javax/inject*" \
-        -and -not -path "./jakarta" \
-        -and -not -path "./jakarta/inject*" \
-        -and -not -path "./org" \
-        -and -not -path "./org/aopalliance" \
-        -and -not -path "./org/aopalliance/aop*" \
-        -and -not -path "./org/aopalliance/intercept*" \
-        -and -not -path "./org/antlr" \
-        -and -not -path "./org/antlr/runtime*" \
-        -and -not -path "./org/objectweb*" \
-        -and -not -path "./org/objectweb/asm*" \
-        -and -not -path "./org/hamcrest*" \
-        -print0 | xargs -0 rm -rf
-    rm -rf ./apex/jorje/lsp
-    popd
-    jar -cvf ${FILENAME_MINIMIZED} -C temp/ .
-    rm -rf temp
+  unzip -d temp ${FILENAME}
+  pushd temp
+  find . -not -path "." \
+    -and -not -path ".." \
+    -and -not -path "./apex*" \
+    -and -not -path "./StandardApex*" \
+    -and -not -path "./messages*" \
+    -and -not -path "./com" \
+    -and -not -path "./com/google" \
+    -and -not -path "./com/google/common*" \
+    -and -not -path "./com/google/inject*" \
+    -and -not -path "./javax" \
+    -and -not -path "./javax/inject*" \
+    -and -not -path "./jakarta" \
+    -and -not -path "./jakarta/inject*" \
+    -and -not -path "./org" \
+    -and -not -path "./org/aopalliance" \
+    -and -not -path "./org/aopalliance/aop*" \
+    -and -not -path "./org/aopalliance/intercept*" \
+    -and -not -path "./org/antlr" \
+    -and -not -path "./org/antlr/runtime*" \
+    -and -not -path "./org/objectweb*" \
+    -and -not -path "./org/objectweb/asm*" \
+    -and -not -path "./org/hamcrest*" \
+    -print0 | xargs -0 rm -rf
+  rm -rf ./apex/jorje/lsp
+  popd
+  jar -cvf ${FILENAME_MINIMIZED} -C temp/ .
+  rm -rf temp
 }
 
 function cleanup() {
-    rm ${FILENAME}
+  rm ${FILENAME}
 }
 
 download
