9ee9eb7a671bdcf774e05617106cdc71d62ebec6
Dang Mai <contact@dangmai.net>
2024-06-30 17:38:41 -0700

    [devcontainer] set java home before settings file is created

--- .devcontainer/set-java-home.sh
+++ .devcontainer/set-java-home.sh
@@ -6,10 +6,17 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SETTINGS_LOCATION="${SCRIPT_DIR}/../.vscode/settings.json"
 TOOL_VERSIONS_LOCATION="${SCRIPT_DIR}/../.tool-versions"
 JAVA_HOME="~/.asdf/installs/java/$(cat "${TOOL_VERSIONS_LOCATION}" | grep "java" | cut -d' ' -f2)"
+
 if [ -f "${SETTINGS_LOCATION}" ];
 then
   jq --arg java_home "$JAVA_HOME" \
     '."salesforcedx-vscode-apex.java.home" = $java_home | ."java.import.gradle.java.home" = $java_home' \
     "${SETTINGS_LOCATION}" > settings.tmp.json \
     && mv settings.tmp.json "${SETTINGS_LOCATION}"
+else
+  # The Workspace Default Settings synchronization hasn't finished yet
+  echo "{
+  \"salesforcedx-vscode-apex.java.home\": \"${JAVA_HOME}\",
+  \"java.import.gradle.java.home\": \"${JAVA_HOME}\"
+}" > "${SETTINGS_LOCATION}"
 fi
