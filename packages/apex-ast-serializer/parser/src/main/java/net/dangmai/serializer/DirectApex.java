d5e027027746f06e0e3493593048e058e9338763
Dang Mai <contact@dangmai.net>
2024-10-06 19:29:46 -0700

    debug print

--- packages/apex-ast-serializer/parser/src/main/java/net/dangmai/serializer/DirectApex.java
+++ packages/apex-ast-serializer/parser/src/main/java/net/dangmai/serializer/DirectApex.java
@@ -5,12 +5,21 @@ import apex.jorje.semantic.compiler.SourceFile;
 import apex.jorje.semantic.compiler.parser.ParserOutput;
 import apex.jorje.semantic.compiler.parser.StandaloneParserEngine;
 import java.io.IOException;
+import org.teavm.jso.JSBody;
 
 public class DirectApex {
 
-  public static void getAST(Boolean anonymous) throws IOException {
-    String sourceCode = "public class Test { }";
+  @JSBody(
+    params = { "message" },
+    script = "console.log(JSON.stringify(message))"
+  )
+  public static native void stringify(String message);
+
+  public static void getAST(String sourceCode, Boolean anonymous)
+    throws IOException {
+    DirectApex.stringify("1");
     SourceFile sourceFile = SourceFile.builder().setBody(sourceCode).build();
+    DirectApex.stringify("2");
     StandaloneParserEngine engine;
     if (anonymous) {
       engine = StandaloneParserEngine.get(
@@ -20,10 +29,14 @@ public class DirectApex {
       engine = StandaloneParserEngine.get(StandaloneParserEngine.Type.NAMED);
     }
     Locations.useIndexFactory(); // without this, comments won't be retained correctly
+    // DirectApex.stringify(engine.parse(sourceFile));
+    // DirectApex.stringify("Hello here");
     ParserOutput output = engine.parse(sourceFile);
+    DirectApex.stringify(output.getUnit().toString());
   }
 
   public static void main(String[] args) throws IOException {
-    getAST(false);
+    String sourceCode = "public class Test { static void main() {} }";
+    getAST(sourceCode, false);
   }
 }
