c9a2a67e0f4c5087ae17cdd7028119d6d5443290
Dang Mai <contact@dangmai.net>
2025-01-01 08:25:59 -0800

    [test] fix integration tests
    Make it easier to find which job failed

--- packages/prettier-plugin-apex/tests_integration/format/run-integration-tests.sh
+++ packages/prettier-plugin-apex/tests_integration/format/run-integration-tests.sh
@@ -5,4 +5,8 @@
 set -euxo pipefail
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-parallel -j0 --line-buffer --no-run-if-empty --link -a "$CURRENT_DIR/test-repos.txt" "$CURRENT_DIR/debug-check-repo.sh"
+if ! parallel -j0 --joblog integration.log --line-buffer --no-run-if-empty --link -a "$CURRENT_DIR/test-repos.txt" "$CURRENT_DIR/debug-check-repo.sh";
+then
+  cat integration.log
+  exit 1
+fi
