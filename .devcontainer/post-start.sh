5e6562654395b12112caa934328ac1f23563d225
Dang Mai <contact@dangmai.net>
2025-07-13 16:51:30 -0700

    correct way to invoke bash with .bash_profile

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -9,7 +9,14 @@ mise exec -- corepack enable
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SETTINGS_LOCATION="${SCRIPT_DIR}/../.vscode/settings.json"
 JAVA_HOME="$(mise where java)"
+
 jq --arg java_home "$JAVA_HOME" \
     '."salesforcedx-vscode-apex.java.home" = $java_home | ."java.import.gradle.java.home" = $java_home | ."java.jdt.ls.java.home" = $java_home' \
+    "${SETTINGS_LOCATION}" > settings.tmp.json \
+    && mv settings.tmp.json "${SETTINGS_LOCATION}"
+
+# Without setting the -l arg, the VSCode tasks are brought up without invoking .bashrc or .bash_profile,
+# which means mise is not activated in the terminal.
+jq '."terminal.integrated.automationProfile.linux" = { "path": "/bin/bash", "args": ["-l"] }' \
     "${SETTINGS_LOCATION}" > settings.tmp.json \
     && mv settings.tmp.json "${SETTINGS_LOCATION}"
