b607c69d49903664a6b631a7a17b7b2ca20e480a
Dang Mai <contact@dangmai.net>
2025-07-13 16:19:09 -0700

    automatically set Java home on container start

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,3 +5,11 @@ set -euxo pipefail
 # This file gets run every time the container starts
 mise install
 mise exec -- corepack enable
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SETTINGS_LOCATION="${SCRIPT_DIR}/../.vscode/settings.json"
+JAVA_HOME="$(mise where java)"
+jq --arg java_home "$JAVA_HOME" \
+    '."salesforcedx-vscode-apex.java.home" = $java_home | ."java.import.gradle.java.home" = $java_home' \
+    "${SETTINGS_LOCATION}" > settings.tmp.json \
+    && mv settings.tmp.json "${SETTINGS_LOCATION}"
